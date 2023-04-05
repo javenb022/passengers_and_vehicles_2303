95bf3333fcd0593c6f6fa09d5b0cf356476061ef
Javen Wilson <javenb022@gmail.com>
2023-04-05 11:52:05 -0400

    add tests for adding passengers to vehicle

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -33,7 +33,26 @@ RSpec.describe Vehicle do
       expect(vehicle.speeding?).to eq(false)
       vehicle.speed
       expect(vehicle.speeding?).to eq(true)
-      
+    end
+
+    it "checks for passengers" do
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+      expect(vehicle.passengers).to eq([])
+    end
+
+    it "adds passengers to vehicle" do
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      jude = Passenger.new({"name" => "Jude", "age" => 20})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)
+      vehicle.add_passenger(taylor)
+
+      expect(vehicle.passengers).to eq([charlie, jude, taylor])
     end
   end
 end
