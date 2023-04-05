a9dc957bd0f27036793b8f9d8a88ef3a8096989e
Javen Wilson <javenb022@gmail.com>
2023-04-05 11:46:05 -0400

    add speeding? and speed methods

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,8 +1,22 @@
 class Vehicle
-  attr_reader :year, :make, :model
-  def initialize(year, make, model)
+  attr_reader :year, :make, :model, :passengers
+  def initialize(year, make, model, passengers = [])
     @year = year
     @make = make
     @model = model
+    @odometer = 0
+    @passengers = []
+  end
+
+  def speeding?
+    if @odometer >= 70
+      true
+    else @odometer < 70
+      false
+    end
+  end
+
+  def speed 
+    @odometer += 70
   end
 end
