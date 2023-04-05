9807a694b740235f7034f949cd1c89a6fb915a20
Javen Wilson <javenb022@gmail.com>
2023-04-05 11:24:03 -0400

    add working tests for name, age and is adult

---
+++ spec/passenger_spec.rb
@@ -0,0 +1,37 @@
+require "./lib/passenger"
+
+RSpec.describe Passenger do
+  describe "initialize" do
+    it "exists" do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      expect(charlie).to be_a(Passenger)
+      expect(taylor).to be_a(Passenger)
+    end
+
+    it "can call passengers name" do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      expect(charlie.name).to eq("Charlie")
+      expect(taylor.name).to eq("Taylor")
+    end
+
+    it "can call passengers age" do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      expect(charlie.age).to eq(18)
+      expect(taylor.age).to eq(12)
+    end
+
+    it "determines if passenger is adult" do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      expect(charlie.adult?).to eq(true)
+      expect(taylor.adult?).to eq(false)
+    end
+  end
+end
